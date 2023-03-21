0f7133f0a58afa9f99877e119881bc053907e854
SakuraEdge <351945458@qq.com>
2023-03-21 17:11:55 +0800

    2023.3.21 17:11

--- src/main/java/com/ifywork/student_springboot/controller/LoginController.java
+++ src/main/java/com/ifywork/student_springboot/controller/LoginController.java
@@ -1,13 +1,25 @@
 package com.ifywork.student_springboot.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class LoginController {
 
+
     @PostMapping("/login")
-    public String login(){
-        return null;
+    public CommonResp<String> login(@RequestBody Map<String,String> map){
+        String role = map.get("role");
+
+        CommonResp<String> commonResp = new CommonResp<>();
+
+        commonResp.setCode(200);
+        commonResp.setMsg("我是超人强，越超人越强");
+        commonResp.setResult(role);
+
+        return commonResp;
     }
 }
