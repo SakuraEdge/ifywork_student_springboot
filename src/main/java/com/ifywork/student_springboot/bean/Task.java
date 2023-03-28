5fe06bfa0b34d10a92834129c115ca1ea3fb39dc
LuMaDaBenBen <1165743986@qq.com>
2023-03-28 09:59:50 +0800

    2023/3/28 9:59

--- src/main/java/com/ifywork/student_springboot/bean/Task.java
+++ src/main/java/com/ifywork/student_springboot/bean/Task.java
@@ -6,71 +6,71 @@ import java.util.Date;
 
 public class Task implements Serializable{
     /** 创建人 */
-    private String createdBy ;
+    private String CREATED_BY ;
     /** 创建时间 */
-    private Date createdTime ;
+    private Date CREATED_TIME ;
     /** 更新人 */
-    private String updatedBy ;
+    private String UPDATED_BY ;
     /** 更新时间 */
-    private Date updatedTime ;
+    private Date UPDATED_TIME ;
     /** 主键 */
     private Integer id ;
     /** 任务发布人id;外键，连接用户表user的ID主键 */
-    private Integer tTeacherUid ;
+    private Integer T_TEACHER_ID ;
     /** 任务执行人id;外键，连接用户表user的ID主键 */
-    private Integer tStudentUid ;
+    private Integer T_STUDENT_ID ;
     /** 任务班级;外键，连接班级表class的ID主键 */
-    private Integer tClassid ;
+    private Integer T_CLASS_ID ;
     /** 任务名称 */
-    private String tName ;
+    private String T_NAME ;
     /** 任务简介 */
-    private String tIntroduce ;
+    private String T_INTRODUCE ;
     /** 任务附件文件夹地址;任务上传在服务器的附件文件夹地址 */
-    private String tAnnexAddress ;
+    private String T_ANNEX_ADDRESS ;
     /** 任务作业上传地址;任务执行者上传作业后保持在服务器的文件地址 */
-    private String tWorkAddress ;
+    private String T_WORK_ADDRESS ;
     /** 任务完成状态 */
-    private String tActive ;
+    private String T_ACTIVE ;
     /** 打分类别;举例说明："110"
      第一个数字1表示需要教师评价，第二个数字表示需要学生自评，第三个数字0表示不需要学生互评
      也就是第一个数字代表是否需要教师评价，第二个数字代表是否需要学生自评，第三个数字代表是否需要学生互评 */
-    private String tScoreType ;
+    private String T_SCORE_TYPE ;
     /** 教师打分 */
-    private Integer tTeacherScore ;
+    private Integer T_TEACHER_SCORE ;
     /** 自评打分 */
-    private Integer tSelfScore ;
+    private Integer T_SELF_SCORE ;
 
     /** 创建人 */
-    public String getCreatedBy(){
-        return this.createdBy;
+    public String getCREATED_BY(){
+        return this.CREATED_BY;
     }
     /** 创建人 */
-    public void setCreatedBy(String createdBy){
-        this.createdBy = createdBy;
+    public void setCREATED_BY(String CREATED_BY){
+        this.CREATED_BY = CREATED_BY;
     }
     /** 创建时间 */
-    public Date getCreatedTime(){
-        return this.createdTime;
+    public Date getCREATED_TIME(){
+        return this.CREATED_TIME;
     }
     /** 创建时间 */
-    public void setCreatedTime(Date createdTime){
-        this.createdTime = createdTime;
+    public void setCREATED_TIME(Date CREATED_TIME){
+        this.CREATED_TIME = CREATED_TIME;
     }
     /** 更新人 */
-    public String getUpdatedBy(){
-        return this.updatedBy;
+    public String getUPDATED_BY(){
+        return this.UPDATED_BY;
     }
     /** 更新人 */
-    public void setUpdatedBy(String updatedBy){
-        this.updatedBy = updatedBy;
+    public void setUPDATED_BY(String UPDATED_BY){
+        this.UPDATED_BY = UPDATED_BY;
     }
     /** 更新时间 */
-    public Date getUpdatedTime(){
-        return this.updatedTime;
+    public Date getUPDATED_TIME(){
+        return this.UPDATED_TIME;
     }
     /** 更新时间 */
-    public void setUpdatedTime(Date updatedTime){
-        this.updatedTime = updatedTime;
+    public void setUPDATED_TIME(Date UPDATED_TIME){
+        this.UPDATED_TIME = UPDATED_TIME;
     }
     /** 主键 */
     public Integer getId(){
@@ -81,95 +81,95 @@ public class Task implements Serializable{
         this.id = id;
     }
     /** 任务发布人id;外键，连接用户表user的ID主键 */
-    public Integer getTTeacherUid(){
-        return this.tTeacherUid;
+    public Integer getT_TEACHER_ID(){
+        return this.T_TEACHER_ID;
     }
     /** 任务发布人id;外键，连接用户表user的ID主键 */
-    public void setTTeacherUid(Integer tTeacherUid){
-        this.tTeacherUid = tTeacherUid;
+    public void setT_TEACHER_ID(Integer T_TEACHER_ID){
+        this.T_TEACHER_ID = T_TEACHER_ID;
     }
     /** 任务执行人id;外键，连接用户表user的ID主键 */
-    public Integer getTStudentUid(){
-        return this.tStudentUid;
+    public Integer getT_STUDENT_ID(){
+        return this.T_STUDENT_ID;
     }
     /** 任务执行人id;外键，连接用户表user的ID主键 */
-    public void setTStudentUid(Integer tStudentUid){
-        this.tStudentUid = tStudentUid;
+    public void setTStudentUid(Integer T_STUDENT_ID){
+        this.T_STUDENT_ID = T_STUDENT_ID;
     }
     /** 任务班级;外键，连接班级表class的ID主键 */
-    public Integer getTClassid(){
-        return this.tClassid;
+    public Integer getT_CLASS_ID(){
+        return this.T_CLASS_ID;
     }
     /** 任务班级;外键，连接班级表class的ID主键 */
-    public void setTClassid(Integer tClassid){
-        this.tClassid = tClassid;
+    public void setT_CLASS_ID(Integer T_CLASS_ID){
+        this.T_CLASS_ID = T_CLASS_ID;
     }
     /** 任务名称 */
-    public String getTName(){
-        return this.tName;
+    public String getT_NAME(){
+        return this.T_NAME;
     }
     /** 任务名称 */
-    public void setTName(String tName){
-        this.tName = tName;
+    public void setT_NAME(String T_NAME){
+        this.T_NAME = T_NAME;
     }
     /** 任务简介 */
-    public String getTIntroduce(){
-        return this.tIntroduce;
+    public String getT_INTRODUCE(){
+        return this.T_INTRODUCE;
     }
     /** 任务简介 */
-    public void setTIntroduce(String tIntroduce){
-        this.tIntroduce = tIntroduce;
+    public void setT_INTRODUCE(String T_INTRODUCE){
+        this.T_INTRODUCE = T_INTRODUCE;
     }
     /** 任务附件文件夹地址;任务上传在服务器的附件文件夹地址 */
-    public String getTAnnexAddress(){
-        return this.tAnnexAddress;
+    public String getT_ANNEX_ADDRESS(){
+        return this.T_ANNEX_ADDRESS;
     }
     /** 任务附件文件夹地址;任务上传在服务器的附件文件夹地址 */
-    public void setTAnnexAddress(String tAnnexAddress){
-        this.tAnnexAddress = tAnnexAddress;
+    public void setT_ANNEX_ADDRESS(String T_ANNEX_ADDRESS){
+        this.T_ANNEX_ADDRESS = T_ANNEX_ADDRESS;
     }
     /** 任务作业上传地址;任务执行者上传作业后保持在服务器的文件地址 */
-    public String getTWorkAddress(){
-        return this.tWorkAddress;
+    public String getT_WORK_ADDRESS(){
+        return this.T_WORK_ADDRESS;
     }
     /** 任务作业上传地址;任务执行者上传作业后保持在服务器的文件地址 */
-    public void setTWorkAddress(String tWorkAddress){
-        this.tWorkAddress = tWorkAddress;
+    public void setT_WORK_ADDRESS(String T_WORK_ADDRESS){
+        this.T_WORK_ADDRESS = T_WORK_ADDRESS;
     }
     /** 任务完成状态 */
-    public String getTActive(){
-        return this.tActive;
+    public String getT_ACTIVE(){
+        return this.T_ACTIVE;
     }
     /** 任务完成状态 */
-    public void setTActive(String tActive){
-        this.tActive = tActive;
+    public void setT_ACTIVE(String T_ACTIVE){
+        this.T_ACTIVE = T_ACTIVE;
     }
     /** 打分类别;举例说明："110"
      第一个数字1表示需要教师评价，第二个数字表示需要学生自评，第三个数字0表示不需要学生互评
      也就是第一个数字代表是否需要教师评价，第二个数字代表是否需要学生自评，第三个数字代表是否需要学生互评 */
-    public String getTScoreType(){
-        return this.tScoreType;
+    public String getT_SCORE_TYPE(){
+        return this.T_SCORE_TYPE;
     }
     /** 打分类别;举例说明："110"
      第一个数字1表示需要教师评价，第二个数字表示需要学生自评，第三个数字0表示不需要学生互评
      也就是第一个数字代表是否需要教师评价，第二个数字代表是否需要学生自评，第三个数字代表是否需要学生互评 */
-    public void setTScoreType(String tScoreType){
-        this.tScoreType = tScoreType;
+    public void setT_SCORE_TYPE(String T_SCORE_TYPE){
+        this.T_SCORE_TYPE = T_SCORE_TYPE;
     }
     /** 教师打分 */
-    public Integer getTTeacherScore(){
-        return this.tTeacherScore;
+    public Integer getT_TEACHER_SCORE(){
+        return this.T_TEACHER_SCORE;
     }
     /** 教师打分 */
-    public void setTTeacherScore(Integer tTeacherScore){
-        this.tTeacherScore = tTeacherScore;
+    public void setT_TEACHER_SCORE(Integer T_TEACHER_SCORE){
+        this.T_TEACHER_SCORE = T_TEACHER_SCORE;
     }
     /** 自评打分 */
-    public Integer getTSelfScore(){
-        return this.tSelfScore;
+    public Integer getT_SELF_SCORE(){
+        return this.T_SELF_SCORE;
     }
     /** 自评打分 */
-    public void setTSelfScore(Integer tSelfScore){
-        this.tSelfScore = tSelfScore;
+    public void setT_SELF_SCORE(Integer T_SELF_SCORE){
+        this.T_SELF_SCORE = T_SELF_SCORE;
     }
 }
