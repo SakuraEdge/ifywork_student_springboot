e7bc6f491b62941d0d1c2c38429f4f879c8734cf
LuMaDaBenBen <1165743986@qq.com>
2023-03-28 09:23:45 +0800

    2023/3/28 9:28

---
+++ src/main/java/com/ifywork/student_springboot/dao/LoginDao.java
@@ -0,0 +1,14 @@
+package com.ifywork.student_springboot.dao;
+
+import com.ifywork.student_springboot.bean.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface LoginDao {
+    @Select("select * from user where uid = ${uid}")
+    User selectUserByUid(@Param("uid") String Uid);
+}
