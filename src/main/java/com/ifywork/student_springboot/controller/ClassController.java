a4054e3d720bf27bb91972491d31d52328b61a15
LuMaDaBenBen <1165743986@qq.com>
2023-03-28 17:05:11 +0800

    2023/3/28 17.05

--- src/main/java/com/ifywork/student_springboot/controller/ClassController.java
+++ src/main/java/com/ifywork/student_springboot/controller/ClassController.java
@@ -4,8 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/task")
-public class TaskController {
-
-
+@RequestMapping("/class")
+public class ClassController {
 }
