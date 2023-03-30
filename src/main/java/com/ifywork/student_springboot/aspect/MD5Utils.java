44199836c4d51fdf7cc387796fb4c2d357840b4a
LuMaDaBenBen <1165743986@qq.com>
2023-03-30 10:10:12 +0800

    2023/3/30 10.10

--- src/main/java/com/ifywork/student_springboot/aspect/MD5Utils.java
+++ src/main/java/com/ifywork/student_springboot/aspect/MD5Utils.java
@@ -4,27 +4,17 @@ import java.security.MessageDigest;
 
 public class MD5Utils {
     public static String getPWD( String str ){
-        StringBuffer sb = new StringBuffer();
-        try{
-            MessageDigest digest = MessageDigest.getInstance("MD5");
-
-            byte[] bs = digest.digest(str.getBytes());
-
-            for (byte b : bs) {
-                int x = b & 255;
-                String s = Integer.toHexString(x);
-                if( x > 0 && x < 16 ){
-                    sb.append("0");
-                    sb.append(s);
-                }else{
-                    sb.append(s);
-                }
+        try {
+            java.security.MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] array = md.digest(str.getBytes("UTF-8"));
+            StringBuilder sb = new StringBuilder();
+            for (byte item : array) {
+                sb.append(Integer.toHexString((item & 0xFF) | 0x100).substring(1, 3));
             }
-
-        }catch( Exception e){
-            System.out.println("加密失败");
+            return sb.toString().toUpperCase();
+        } catch (Exception exception) {
         }
-        return sb.toString();
+        return null;
     }
 
 }
