ca51117ee2e980976b7c95a5ae2943ee783fd0d8
LuMaDaBenBen <1165743986@qq.com>
2023-04-04 16:18:56 +0800

    2023/4/4 16:18

---
+++ springboot/src/main/java/com/ifywork/student_springboot/controller/FileUploadController.java
@@ -0,0 +1,50 @@
+package com.ifywork.student_springboot.controller;
+
+
+import com.ifywork.student_springboot.aspect.CommonResp;
+import com.ifywork.student_springboot.aspect.DataIsNull;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+public class FileUploadController {
+    private String fileSavePath = "E:\\Test";
+    ArrayList<String> mylist=new ArrayList();
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd/");
+
+    @RequestMapping("/uploads")
+    public CommonResp<List<String>> upload(MultipartFile[] uploadFiles, HttpServletRequest req) throws IOException {
+        DataIsNull<String> dataIsNull = new DataIsNull<String>();
+        return dataIsNull.listIsNull(uoload(uploadFiles));
+    }
+
+    @Async
+     public List<String>  uoload(MultipartFile[] files) throws IOException {
+        List<String> paths = new ArrayList<>();
+        if (files.length > 0){
+            for (MultipartFile file : files) {
+                if (!file.isEmpty()){
+                    String originalFilename = file.getOriginalFilename();
+                    SimpleDateFormat sdf= new SimpleDateFormat("yyyyMMddHHmmss");
+                    String path = "E:\\Test\\"+ sdf.format(System.currentTimeMillis()) + "_" + originalFilename;
+                    paths.add(path);
+                    file.transferTo(new File(path));
+                }
+            }
+        }
+        return  paths;
+      }
+}
