757552fcee181232a65a41d4377cbb8f5e88ba29
LuMaDaBenBen <1165743986@qq.com>
2023-04-04 17:07:44 +0800

    2023/4/4 17:07

--- springboot/src/main/java/com/ifywork/student_springboot/StudentSpringbootApplication.java
+++ springboot/src/main/java/com/ifywork/student_springboot/StudentSpringbootApplication.java
@@ -2,6 +2,12 @@ package com.ifywork.student_springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.util.unit.DataSize;
+
+import javax.servlet.MultipartConfigElement;
+
 
 @SpringBootApplication
 public class StudentSpringbootApplication {
@@ -9,4 +15,16 @@ public class StudentSpringbootApplication {
     public static void main(String[] args) {
         SpringApplication.run(StudentSpringbootApplication.class, args);
     }
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //文件最大
+        factory.setMaxFileSize(DataSize.parse("100MB"));
+        // 设置总上传数据总大小
+        factory.setMaxRequestSize(DataSize.parse("100MB"));
+        return factory.createMultipartConfig();
+    }
 }
+
